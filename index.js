a1434bbcf04b8bd03266f8b1d6e5ddc854a28868
FazleRahmanEvan <evanshakib26@gmail.com>
2022-05-14 01:04:52 +0600

    inventory data connected

--- index.js
+++ index.js
@@ -14,12 +14,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqabw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Motor Bike Db Connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+     try{
+        await client.connect();
+        const inventoryCollection = client.db('motorBike').collection('inventory');
+        app.get('/inventory', async(req,res) =>{
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventory = await cursor.toArray();
+            res.send(inventory);
+        
+        });
+     }
+     finally{
+
+     }
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req,res) => {
