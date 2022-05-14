73ca1e803ec938dc91502610fbfe112a5bb13b5f
FazleRahmanEvan <evanshakib26@gmail.com>
2022-05-15 00:19:29 +0600

    adding heroku

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ async function run(){
            const result = await inventoryCollection.deleteOne(query);
            res.send(result);
        })
+       app.replacement('/inventory/:id', async(req,res) => {
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};
+           const result = await inventoryCollection.replaceOne(query);
+           res.send(result);
+       })
 
       }
       finally{
