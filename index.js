ac10303a4f69c1b0e205cc67eb0d4da856c9caae
FazleRahmanEvan <evanshakib26@gmail.com>
2022-05-14 13:20:02 +0600

    post commit

--- index.js
+++ index.js
@@ -10,42 +10,50 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqabw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
-     try{
-        await client.connect();
-        const inventoryCollection = client.db('motorBike').collection('inventory');
-        app.get('/inventory', async(req,res) =>{
-            const query = {};
-            const cursor = inventoryCollection.find(query);
-            const inventory = await cursor.toArray();
-            res.send(inventory);
-        
-        });
+      try{
+          await client.connect();
+          const inventoryCollection = client.db('motorBike').collection('inventory');
 
-        app.get('/inventory/:id', async(req,res) => {
-            const id =req.params.id;
-            const query ={_id: ObjectId(id)};
-            const inventory = await inventoryCollection.findOne(query);
-            res.send(inventory);
-        })
-     }
-     finally{
+       app.get('/inventory', async (req,res) => {
+        const query ={};
+        const cursor = inventoryCollection.find(query);
+        const inventorys = await cursor.toArray();
+        res.send(inventorys);
+       });
 
-     }
+       app.get('/inventory/:id', async(req,res) =>{
+           const id = req.params.id;
+           const query= {_id: ObjectId(id)};
+           const inventory = await inventoryCollection.findOne(query);
+           res.send(inventory);
+       });
+
+       //post
+
+       app.post('/inventory', async(req, res) => {
+           const newInventory = req.body;
+           const result = await inventoryCollection.insertOne(newInventory);
+           res.send(result);
+       })
+
+      }
+      finally{
+
+      }
 }
 
 run().catch(console.dir);
 
 
 
-app.get('/', (req,res) => {
-    res.send('Running bike Server');
-})
+app.get('/',(req,res)=>{
+    res.send('Server Running');
+});
+
 app.listen(port,() =>{
-    console.log('listening',port);
+    console.log('Listening to port', port)
 })
